e7b33ca2a2c73dbc83a3df2354fc2b63c9007c82
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-10-20 19:25:24 +0100

    Fixed random coin placement

--- js/game.js
+++ js/game.js
@@ -3,21 +3,36 @@ class Main {
     this.load.image('player', 'assets/player.png');
     this.load.image('wallV', './assets/wallVertical.png');
     this.load.image('wallH', './assets/wallHorizontal.png');
+    this.load.image('coin', './assets/coin.png');
   }
 
   create() {
     this.player = this.physics.add.sprite(250, 170, 'player');
+    this.coin = this.physics.add.sprite(60, 130, 'coin');
+    this.scoreLabel = this.add.text(30, 25, 'Score: 0',
+    { font: '18px Arial', fill: '#fff' });
+    this.score = 0
+
     this.player.body.gravity.y = 500;
+
     this.arrow = this.input.keyboard.createCursorKeys();
+
     this.createWorld();
   }
 
   update() {
     this.physics.collide(this.player, this.walls);
     this.movePlayer();
+
     if (this.player.y > 340 || this.player.y < 0) {
       this.playerDie();
     }
+
+    if (this.physics.overlap(this.player, this.coin)) {
+      this.takeCoin();
+      this.updateCoinPosition();
+    }
+
   }
 
   movePlayer() {
@@ -53,6 +68,28 @@ class Main {
 
   playerDie() {
     this.scene.start('main')
+  }
+
+  takeCoin() {
+    this.score +=5;
+    this.scoreLabel.setText('Score: ' + this.score);
+  }
+
+  updateCoinPosition() {
+    let positions = [
+      { x: 140, y: 60 },
+      { x: 360, y: 60 },
+      { x: 60, y: 140 },
+      { x: 440, y: 140 },
+      { x: 130, y: 300 },
+      { x: 370, y: 300 },
+    ];
+
+    positions = positions.filter(coin => coin.x !== this.coin.x);
+
+    let newPosition = Phaser.Math.RND.pick(positions);
+
+    this.coin.setPosition(newPosition.x, newPosition.y);
   }
 }
 
